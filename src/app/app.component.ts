9dbd34cda6ad56b7cdfb254b142f4af6e8224b0d
unknown <burellert@gmail.com>
2024-09-22 21:45:28 +0200

    ajout choix langue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import {TranslateModule} from "@ngx-translate/core";
+import {TranslateService} from "@ngx-translate/core";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  constructor(private translateService: TranslateService) {
+    this.translateService.addLangs(['fr', 'en']);
+    this.translateService.setDefaultLang(localStorage.getItem('lang') || 'fr');
+    this.translateService.use('fr');
+  }
+
   title = 'tb-dev';
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule} from '@angular/forms';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCardModule} from '@angular/material/card';
+import { TranslateService } from '@ngx-translate/core';
+
+
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatRadioModule, FormsModule, MatCheckboxModule, MatSlideToggleModule, MatCardModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+
+
+export class HeaderComponent implements OnInit {
+
+  lang: string = 'fr';
+  isChecked = false;
+
+  constructor(private translateService: TranslateService) {
+  }
+
+  ngOnInit(): void {
+    this.lang = localStorage.getItem('lang') || 'fr';
+  }
+
+  onChange(choice: boolean) {
+    console.log(choice);
+   if(choice) {
+    this.lang  = 'en';
+    localStorage.setItem('lang', this.lang);
+    this.translateService.use('en');
+
+  } else {
+    this.lang = 'fr';
+    localStorage.setItem('lang', this.lang);
+    this.translateService.use('fr');
+
+  }
+}
+
+}
