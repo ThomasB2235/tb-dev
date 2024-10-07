263d4226090a32137ac040e30602be5a8fe5fd3b
unknown <burellert@gmail.com>
2024-10-07 20:44:55 +0200

    pacman done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
@@ -30,11 +30,68 @@ export interface Tile {
 })
 export class AppComponent {
 
+  isOriginalCursor = true;
+  title = 'Thomas Bureller';
+
+   // Utilisation d'un tableau pour garder une trace de l'état des fantômes
+   ghosts: { id: string; exploded: boolean }[];
+
   constructor(private translateService: TranslateService) {
     this.translateService.addLangs(['fr', 'en']);
     this.translateService.setDefaultLang(localStorage.getItem('lang') || 'fr');
     this.translateService.use('fr');
+    this.ghosts = [
+      { id: 'aGhost', exploded: false },
+      { id: 'bGhost', exploded: false },
+      { id: 'cGhost', exploded: false },
+      { id: 'dGhost', exploded: false },
+      { id: 'eGhost', exploded: false },
+    ];
+
   }
 
-  title = 'Thomas Bureller';
+    // Event Listener pour le mousedown (quand on clique)
+    @HostListener('mousedown', ['$event'])
+    onMouseDown(event: MouseEvent) {
+      const divElement = document.querySelector('.relative') as HTMLElement;
+      // Change le curseur pendant le clic
+      divElement.style.cursor = "url('/assets/images/cursor/pacmanCursorClick.png'), auto";
+    }
+
+    // Event Listener pour le mouseup (quand on relâche le clic)
+    @HostListener('mouseup', ['$event'])
+    onMouseUp(event: MouseEvent) {
+      const divElement = document.querySelector('.relative') as HTMLElement;
+      // Remet le curseur à son état initial après le clic
+      divElement.style.cursor = "url('/assets/images/cursor/pacmanCursor.png'), auto";
+    }
+// Méthode qui applique l'explosion à la div cliquée
+explodeGhost(event: MouseEvent) {
+  const ghostElement = event.currentTarget as HTMLElement;
+
+  // Vérifie si l'élément a déjà explosé
+  const ghostId = ghostElement.classList[1]; // Récupère l'ID du fantôme
+  const ghost = this.ghosts.find(ghost => ghost.id === ghostId);
+
+  if (ghost && !ghost.exploded) {
+    ghostElement.classList.add('explode');
+    ghost.exploded = true;
+    setTimeout(() => {
+      ghostElement.remove();
+    }, 200);
+  }
 }
+
+handleGhostClick(event: MouseEvent) {
+  const target = event.target as HTMLElement;
+  const ghostElement = target.closest('.ghost');
+  if (ghostElement) {
+    this.explodeGhost({ currentTarget: ghostElement } as unknown as MouseEvent);
+  }
+
+}
+  }
+
+
+
+
